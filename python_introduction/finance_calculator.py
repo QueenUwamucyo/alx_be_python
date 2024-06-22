1ab89d61a84a15cf335fb10675c9d6fa9ed01497
Queen Uwamucyo <queenwamucyo@gmail.com>
2024-06-22 14:55:06 +0200

    advanced task

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,8 @@
+monthly_income = float(input("Enter your monthly salary: "))
+monthly_expense = float(input("Enter your total monthly expense: "))
+monthly_savings = monthly_income - monthly_expense
+annual_savings_without_inerest = monthly_savings * 12
+annual_interest = annual_savings_without_inerest * 0.05
+projected_annual_savings = annual_savings_without_inerest + annual_interest
+print(f"Your monthly savings are ${monthly_savings:.2f}.")
+print(f"Projected savings after one year, with interest, is: ${projected_annual_savings:.2f}.")
