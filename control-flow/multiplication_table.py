002e4acbfa98ae75b9ee560b1b4ff21a30009d00
Queen Uwamucyo <queenwamucyo@gmail.com>
2024-06-27 12:12:42 +0200

    task 1

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -4,11 +4,12 @@ def main():
         number = int(input("Enter a number to see its multiplication table: "))
         
         
-        for i in range(1, 11):
-            product = number * i
-            print(f"{number} * {i} = {product}")
+        for i in range(1, 10+1):
+            print(f"{number} * {i} = {number * i}")
     except ValueError:
         print("Invalid input. Please enter a numerical value.")
+
 if __name__ == "__main__":
     main()
+
 
