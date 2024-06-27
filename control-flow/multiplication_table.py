e010580132c953160ecbf711e868bc374a540979
Queen Uwamucyo <queenwamucyo@gmail.com>
2024-06-27 11:56:55 +0200

    task 2

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,7 +1,9 @@
 def main():
     try:
-    
+        
         number = int(input("Enter a number to see its multiplication table: "))
+        
+        
         for i in range(1, 11):
             product = number * i
             print(f"{number} * {i} = {product}")
@@ -10,3 +12,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
